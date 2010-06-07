fc5526d656c1f4fefecd85bb78a5f361217193a1
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2010-06-07 20:09:46 +0530

    Using ClassPathSuite to run all tests from ant

---
+++ test/com/jaliansystems/simpletemplate/AllTests.java
@@ -0,0 +1,9 @@
+package com.jaliansystems.simpletemplate;
+
+import org.junit.extensions.cpsuite.ClasspathSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+public class AllTests {
+
+}
