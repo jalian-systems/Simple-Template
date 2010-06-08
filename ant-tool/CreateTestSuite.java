1e72eeb81ac5ebd02e94b46ecdc8a3e52aae4a61
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2010-06-08 18:28:17 +0530

    Removed cpsuite*.jar as it is GPL'ed and simple template is going to be Apache License

---
+++ ant-tool/CreateTestSuite.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.FileFilter;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateTestSuite {
+
+	static String code_prefix = 
+		"import org.junit.runner.RunWith;\n" +
+		"import org.junit.runners.Suite;\n" +
+		"\n" +
+		" @RunWith(Suite.class)\n" +
+		" @Suite.SuiteClasses({\n" +
+		"\n" ;
+
+	static String code_suffix = 
+		"    })\n" +
+		"public class AllTests {\n" +
+		"}\n" +      
+		"\n" ;
+
+	private static String sourceDirectory;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		if (args.length != 2) {
+			System.err.println("Usage: CreateTestSuite sourceDirectory fileNamePattern");
+			System.exit(1);
+		}
+		ArrayList<String> testClassNames = new ArrayList<String>();
+		sourceDirectory = args[1];
+		File currentDirectory = new File(sourceDirectory);
+		String filePattern = args[0];
+		collectFiles(testClassNames, currentDirectory, filePattern);
+		PrintStream writer = new PrintStream(new FileOutputStream("AllTests.java"));
+		writer.println(code_prefix);
+		for (int i = 0; i < testClassNames.size(); i++) {
+			String className = testClassNames.get(i);
+			writer.print("    " + className
+					+ ".class");
+			if (i < testClassNames.size() - 1)
+				writer.println(",");
+			else
+				writer.println();
+		}
+		writer.println(code_suffix);
+		writer.close();
+		System.out.println(testClassNames);
+	}
+
+	private static void collectFiles(ArrayList<String> testFileNames,
+			File currentDirectory, final String filePattern) {
+		File[] listFiles = currentDirectory.listFiles(new FileFilter() {
+			public boolean accept(File pathname) {
+				return pathname.isFile()
+						&& pathname.getName().matches(filePattern);
+			}
+		});
+		List<File> asList = Arrays.asList(listFiles);
+		for (File file : asList) {
+			testFileNames.add(getRelative(file.getAbsolutePath()));
+		}
+		File[] listDirs = currentDirectory.listFiles(new FileFilter() {
+
+			public boolean accept(File pathname) {
+				return pathname.isDirectory();
+			}
+		});
+		for (File file : listDirs) {
+			collectFiles(testFileNames, file, filePattern);
+		}
+	}
+
+	private static String getRelative(String absolutePath) {
+		if (absolutePath.startsWith(sourceDirectory))
+			return absolutePath.substring(sourceDirectory.length() + 1,
+					absolutePath.length() - 5).replaceAll("\\/", ".");
+		return null;
+	}
+}
